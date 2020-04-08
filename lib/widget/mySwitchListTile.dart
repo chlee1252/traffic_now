4aeb3ccbdfab6e43c917c446e61a676fd8f7133b
chlee1252 <chlee1252@gmail.com>
2020-04-07 23:18:06 -0700

    Created CupertinoStyleSwitch widget and ListTile

---
+++ lib/widget/mySwitchListTile.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:trafficnow/widget/cupertinoSwitchListTile.dart';
+
+class MySwitchListTile extends StatefulWidget {
+  MySwitchListTile({this.title});
+
+  final String title;
+
+  @override
+  _MySwitchListTileState createState() => _MySwitchListTileState();
+}
+
+class _MySwitchListTileState extends State<MySwitchListTile> {
+  bool _value = false;
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoSwitchListTile(
+      value: _value,
+      onChanged: (value) {
+        setState(() {
+          _value = value;
+        });
+      },
+      activeColor: CupertinoColors.activeGreen,
+      title: Text(widget.title),
+    );
+  }
+}
