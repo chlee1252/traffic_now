c09432d818a4ee2371774f6b38677466bfc7a707
chlee1252 <chlee1252@gmail.com>
2020-04-14 00:32:11 -0700

    minor fixes

--- lib/screens/addScheduleScreen.dart
+++ lib/screens/addScheduleScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:intl/intl.dart';
-import 'package:trafficnow/module/userPlace.dart';
 
 class AddScheduleScreen extends StatefulWidget {
 
