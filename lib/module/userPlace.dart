0517a17b42153afd84ea33c1e8783f17b09c253d
chlee1252 <chlee1252@gmail.com>
2020-04-14 00:30:26 -0700

    implemented to convert JSON format

--- lib/module/userPlace.dart
+++ lib/module/userPlace.dart
@@ -4,12 +4,27 @@ class UserPlace {
   String dest;
   String startID;
   String destID;
+  bool turnON;
 
-  UserPlace({DateTime date, String startPoint, String dest, String startID, String destID}) {
+  UserPlace({DateTime date, String startPoint, String dest, String startID, String destID, bool turnON=false}) {
     this.date = date;
     this.startPoint = startPoint;
     this.dest = dest;
     this.startID = startID;
     this.destID = destID;
+    this.turnON = turnON;
   }
+
+  supportJSON() {
+    Map<String, dynamic> map = new Map();
+    map['date'] = this.date.toString();
+    map['start'] = this.startPoint;
+    map['dest'] = this.dest;
+    map['startID'] = this.startID;
+    map['destID'] = this.destID;
+    map['turnON'] = this.turnON;
+
+    return map;
+  }
+
 }
