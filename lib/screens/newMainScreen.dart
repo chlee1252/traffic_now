ac40c71f345ba6731011fba1f667cc1df135452d
chlee1252 <chlee1252@gmail.com>
2020-04-23 00:09:09 -0700

    Refactor Alarm Tab

--- lib/screens/newMainScreen.dart
+++ lib/screens/newMainScreen.dart
@@ -1,6 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
 import 'package:bubble_bottom_bar/bubble_bottom_bar.dart';
+import 'package:trafficnow/module/userPlace.dart';
 import 'package:trafficnow/screens/placeInputScreen.dart';
+import 'package:trafficnow/widget/alarmTile.dart';
 
 class NewMainScreen extends StatefulWidget {
   static final String id = "NewMainScreen";
@@ -10,15 +15,21 @@ class NewMainScreen extends StatefulWidget {
 }
 
 class _NewMainScreenState extends State<NewMainScreen> {
+  UserPlace _userPlace;
   int _currentIndex = 0;
+  bool _value = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add_alarm),
-        onPressed: () {
-          Navigator.pushNamed(context, PlaceInputScreen.id);
+        onPressed: () async {
+          final result =
+              await Navigator.pushNamed(context, PlaceInputScreen.id);
+          setState(() {
+            _userPlace = result;
+          });
         },
         backgroundColor: Color.fromRGBO(219, 235, 196, 1.0),
         foregroundColor: Colors.black,
@@ -33,39 +44,102 @@ class _NewMainScreenState extends State<NewMainScreen> {
                 child: Placeholder(),
               ),
             ),
-            Expanded(
+            Container(
+              height: MediaQuery.of(context).size.height * 0.20,
               child: IndexedStack(
                 index: _currentIndex,
                 children: [
                   Card(
                     elevation: 3.0,
-                    color: Colors.grey,
+                    color: Colors.white,
                     shadowColor: Colors.black,
                     shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.grey, width: 2),
                       borderRadius: BorderRadius.circular(15.0),
                     ),
-                    child:
-                    Container(),
+                    child: _userPlace == null
+                        ? Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(40.0),
+                              child: AutoSizeText(
+                                "Please add Destination :)",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                                maxLines: 1,
+                              ),
+                            ),
+                          )
+                        : Row(
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.15,
+                                child: Center(
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left: 8.0),
+                                    child: Icon(
+                                      Icons.location_on,
+                                      size: 35.0,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 10.0),
+                                  child: AutoSizeText(
+                                    _userPlace.dest,
+                                    style: TextStyle(
+                                        fontSize: 20.0,
+                                        fontWeight: FontWeight.bold),
+                                    maxLines: 1,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                   ),
                   Card(
                     elevation: 3.0,
-                    color: Colors.red,
+                    color: Colors.white,
                     shadowColor: Colors.black,
                     shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.grey, width: 2.0),
                       borderRadius: BorderRadius.circular(15.0),
                     ),
-                    child:
-                    Container(),
+                    child: _userPlace == null
+                        ? Center(
+                            child: Padding(
+                              padding: const EdgeInsets.all(40.0),
+                              child: AutoSizeText(
+                                "Please add Schedule :)",
+                                style: TextStyle(
+                                    fontSize: 20.0,
+                                    fontWeight: FontWeight.bold),
+                                maxLines: 1,
+                              ),
+                            ),
+                          )
+                        : AlarmTile(
+                            value: _userPlace.turnON,
+                            date: _userPlace.date,
+                            onChanged: (value) {
+                              setState(() {
+                                _userPlace.turnON = value;
+                              });
+                            },
+                          ),
                   ),
                   Card(
                     elevation: 3.0,
-                    color: Colors.green,
+                    color: Colors.white,
                     shadowColor: Colors.black,
                     shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.grey, width: 2.0),
                       borderRadius: BorderRadius.circular(15.0),
                     ),
-                    child:
-                    Container(),
+                    child: Container(),
                   ),
                 ],
               ),
