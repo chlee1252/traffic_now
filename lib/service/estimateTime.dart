3a52c4365d902cd60f40d10349a4f9268fb89160
chlee1252 <chlee1252@gmail.com>
2020-04-25 22:00:44 -0700

    Added Getting steps function

--- lib/service/estimateTime.dart
+++ lib/service/estimateTime.dart
@@ -9,10 +9,35 @@ class EstimateTime {
 
   final UserPlace userData;
   final LatLng userGeo;
+//  Set<LatLng> navigation = {};
+
+  Future<Set> getSteps() async {
+    Set<LatLng> navigation = {};
+    final String url =
+        "https://maps.googleapis.com/maps/api/directions/json?departure_time=now&origin=${this.userGeo.latitude}, ${this.userGeo.longitude}&destination=place_id:${this.userData.destID}&key=$myKey";
+    var response = await http.get(url, headers: {
+      "Accept": "application/json",
+    });
+
+    if (response.statusCode == 200) {
+      var data = jsonDecode(response.body);
+      try {
+        var steps = data['routes'][0]['legs'][0]['steps'];
+        steps.forEach((value) {
+          navigation.add(LatLng(value['start_location']['lat'], value['start_location']['lng']));
+          navigation.add(LatLng(value['end_location']['lat'], value['end_location']['lng']));
+        });
+      } catch(e) {
+        print(e);
+      }
+    }
+
+    return navigation;
+  }
+
   Future<UserPlace> getEstimate() async {
     final String url =
         "https://maps.googleapis.com/maps/api/directions/json?departure_time=now&origin=${this.userGeo.latitude}, ${this.userGeo.longitude}&destination=place_id:${this.userData.destID}&key=$myKey";
-
     var response = await http.get(url, headers: {
       "Accept": "application/json",
     });
@@ -26,6 +51,7 @@ class EstimateTime {
         var time = data["routes"][0]["legs"][0]["duration"]['text'];
         userData.estTime = time;
       }
+
     }
 
     return userData;
