e4140c53c9147c2e4d6c294e0bad7b5b75ec4fa7
chlee1252 <chlee1252@gmail.com>
2020-06-02 00:53:12 -0700

    Refactoring Code

---
+++ lib/widget/mainScreenWidgets.dart
@@ -0,0 +1,6 @@
+export 'package:trafficnow/widget/src/alarmTile.dart';
+export 'package:trafficnow/widget/src/destTile.dart';
+export 'package:trafficnow/widget/src/emptyTile.dart';
+export 'package:trafficnow/widget/src/estTile.dart';
+export 'package:trafficnow/widget/src/infoCard.dart';
+export 'package:trafficnow/widget/src/myBottomNav.dart';
