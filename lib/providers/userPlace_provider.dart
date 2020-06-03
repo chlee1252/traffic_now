51e5254d94acd2abe4c381a3e801e0480fce9c95
chlee1252 <chlee1252@gmail.com>
2020-06-02 23:52:22 -0700

    UserPlace Provider Initial Commit :tada:

---
+++ lib/providers/userPlace_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/foundation.dart';
+import 'package:trafficnow/module/userPlace.dart';
+import 'package:trafficnow/storage/storage.dart';
+
+class UserPlaceProvider extends ChangeNotifier {
+  UserPlace _userPlace;
+
+  UserPlace getUserData() => _userPlace;
+
+  void setUserData(UserPlace userPlace) {
+    _userPlace = userPlace;
+  }
+
+  void localStorage() {
+    Storage storage = new Storage();
+    storage.isReady().then((data) {
+      if (data) {
+        _userPlace = storage.getItems();
+      } else {
+        _userPlace = null;
+      }
+    });
+    
+  }
+}
