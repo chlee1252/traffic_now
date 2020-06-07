332a20d3b0ceb2aed30a0b9e48d1c6b7092afcbe
chlee1252 <chlee1252@gmail.com>
2020-06-07 02:00:44 -0700

    Export providers that needed on MainScreen

---
+++ lib/providers/mainProviderExport.dart
@@ -0,0 +1,3 @@
+export 'package:trafficnow/providers/markerProvider.dart';
+export 'package:trafficnow/providers/polylineProvider.dart';
+export 'package:trafficnow/providers/userPlaceProvider.dart';
